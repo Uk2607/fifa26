e23984a8b31ce31034bc065f8f6b204dc7f4d777
Utkarsh Kumawat <utkarsh2600@gmail.com>
2026-07-05 00:29:13 +0530

    feat: update KO-90 match scores and status to locked

--- src/constants/presetScores.js
+++ src/constants/presetScores.js
@@ -512,11 +512,11 @@ export const PRESET_SCORES = {
     timestamp: "2026-07-05T02:30:00+05:30", status: "open"
   },
   "KO-90": { // Canada vs Morocco
-    score1: 0, score2: 0,
+    score1: 0, score2: 2,
     penalty1: null, penalty2: null,
-    yellow1: 2, yellow2: 4, secondYellow1: 0, secondYellow2: 0,
+    yellow1: 4, yellow2: 4, secondYellow1: 0, secondYellow2: 0,
     red1: 0, red2: 0,
-    timestamp: "2026-07-04T22:30:00+05:30", status: "open"
+    timestamp: "2026-07-04T22:30:00+05:30", status: "locked"
   },
   "KO-91": { // Brazil vs Norway
     score1: null, score2: null,
