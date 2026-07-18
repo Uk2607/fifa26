10c3eb701f0ada19801012cd5c58a2dde9a12b18
Utkarsh Kumawat <utkarsh2600@gmail.com>
2026-07-19 03:21:12 +0530

    feat: update KO-103 match score

--- src/constants/presetScores.js
+++ src/constants/presetScores.js
@@ -603,7 +603,7 @@ export const PRESET_SCORES = {
     timestamp: "2026-07-16T00:30:00+05:30", status: "locked"
   },
   "KO-103": { // France vs England
-    score1: null, score2: null,
+    score1: 0, score2: 4,
     penalty1: null, penalty2: null,
     yellow1: 0, yellow2: 0, secondYellow1: 0, secondYellow2: 0,
     red1: 0, red2: 0,
